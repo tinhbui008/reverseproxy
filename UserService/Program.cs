ef23598f0168e8b4240746ace304f0c94b92b197
Tinh Bui <buitrungtinh130296@gmail.com>
2023-11-16 13:35:37 +0700

    cm

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.Services.AddOcelot(builder.Configuration);
+//builder.Services.AddOcelot(builder.Configuration); 
 //builder.Services.AddCors(options =>
 //{
 //    options.AddPolicy("CorsPolicy",
