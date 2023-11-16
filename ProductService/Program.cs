fdf66df9767992bd4600717eba482e4be12b50f8
Tinh Bui <buitrungtinh130296@gmail.com>
2023-11-16 08:58:01 +0700

    cm

--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -6,7 +6,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        builder => builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
 var app = builder.Build();
 
 
