4816f9efd56b56d9c84e3136111932de112f6325
Tinh Bui <buitrungtinh130296@gmail.com>
2023-11-16 09:16:27 +0700

    cm

--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -1,3 +1,4 @@
+using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,9 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddOcelot(builder.Configuration);
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy",
--- UserService/Program.cs
+++ UserService/Program.cs
@@ -1,7 +1,12 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddOcelot(builder.Configuration);
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy",
